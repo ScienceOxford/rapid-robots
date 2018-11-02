8a9e8af8f8c70e8fede7182afb63534ddb5601db
Sarah Townson <sarah.townson@scienceoxford.com>
2018-11-02 15:39:51 +0000

    Create robot_transmitter.py

---
+++ robot_transmitter.py
@@ -0,0 +1,24 @@
+from microbit import *
+import radio
+
+# Please tag us if used!
+# We'd love to see what you make:
+# @ScienceOxford
+
+radio.on()
+radio.config(channel=6)
+
+while True:
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.show(Image.ARROW_N)
+        radio.send('forward')
+    elif button_a.is_pressed():
+        radio.send('left')
+        display.show(Image.ARROW_W)
+    elif button_b.is_pressed():
+        radio.send('right')
+        display.show(Image.ARROW_E)
+    else:
+        radio.send('stop')
+        display.clear()
+    sleep(1)
